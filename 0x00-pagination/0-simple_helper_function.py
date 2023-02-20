62651092d4b218685db7aa7f694674174835a902
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-02-20 14:16:47 +0100

    Update 0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,9 +2,10 @@
 """
     contains function index_range
 """
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
         Args:
             page: int
