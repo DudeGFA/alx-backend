f22e43ac1d5076e414f1f8ec3b5ec7852b02ca68
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-02-20 14:13:27 +0100

    update 0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -14,4 +14,4 @@ def index_range(page: int, page_size: int) -> tuple[int, int]:
     """
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
-    return tuple([start_index, end_index])
+    return (start_index, end_index)
